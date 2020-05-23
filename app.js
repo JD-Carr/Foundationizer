952d7d9e91b2f80ac2ca16033bb4ba2932cf74d1
JD-Carr <jordancarr@gmail.com>
2020-05-23 18:46:29 -0500

    creating the github repo

---
+++ app.js
@@ -0,0 +1,92 @@
+const {app, BrowserWindow, Menu} = require('electron')
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+
+let menu;
+let appWindow;
+
+const menubarTop = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New',
+        click: () => {
+          console.log("Pre-New File");
+          appWindow.loadFile('src/worldbuilder.html');
+          console.log("Post-New File");
+        }
+      },
+      {
+        label: 'Open',
+        click: () => {
+          console.log("Pre-New File");
+          console.log("Post-New File");
+        }
+      },
+      {
+        label: 'Save',
+        click: ()=> {
+          console.log("Pre-Save File");
+          console.log("Post-Save File");
+        }
+      }
+    ]
+  },
+  {
+    label: 'Toggle Developer Tools',
+    accelerator: (function() {
+      return 'Ctrl+Shift+I';
+    })(),
+    click: function(item, focusedWindow) {
+      console.log("Pre-Toggle Developer Tools");
+      if (focusedWindow)
+      focusedWindow.toggleDevTools();
+      console.log("Post-Toggle Developer Tools");
+    }
+  },
+  {
+    label: 'Quit',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'Quit',
+  }
+];
+
+function createWindow() {
+  appWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    icon: 'assets/favicon.ico',
+    backgroundColor: '#0000',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  appWindow.loadFile('index.html');
+
+  menu = Menu.buildFromTemplate(menubarTop);
+  Menu.setApplicationMenu(menu);
+
+  appWindow.webContents.openDevTools();
+
+  appWindow.on('closed', function () {
+    appWindow = null;
+  })
+}//end createWindow
+
+app.on('ready', createWindow);
+
+app.on('resize', function(e,x,y){
+  appWindow.setSize(x, y)
+})
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
+
+app.on('activate', function () {
+  if (appWindow === null) {
+    createWindow()
+  }
+})
---
+++ src/select_nation_options.js
@@ -0,0 +1,53 @@
+'use strict';
+
+console.log('INIT_SELECT_NATION_OPTIONS_START');
+
+const fs = require("fs");
+
+var nationOptionsObject = {};
+var nationOptionsKeyArray = [];
+
+var nationOptionsObject = JSON.parse(fs.readFileSync('lib/nation_options.json', "utf-8"));
+console.log(nationOptionsObject);
+
+var nationOptionsKeyArray = Object.keys(nationOptionsObject);
+console.log(nationOptionsKeyArray);
+
+loadSelectionNationOptions(nationOptionsObject, nationOptionsKeyArray);
+
+//This pulls the data from the JSON files so they can be added to selec option boxes.
+function loadSelectionNationOptions (optionsObject, optionsKeyArray) {
+  let key;
+  for (key in nationOptionsObject) {
+    let selectionID = addSelect(sentenceCase(key));
+    let value;
+    for (value in nationOptionsObject[key]) {
+      addSelectionNationOptions(selectionID, nationOptionsObject[key][value]);
+    }
+  }
+}
+
+//This adds the values from the JSON files to select box options.
+function addSelectionNationOptions (select_id, text) {
+  var select = document.getElementById(select_id);
+  select.options[select.options.length] = new Option(text);
+}
+
+//This is a thingamajig so I can dynamically read key values from JSON files. Will fix this to instead dynamically CREATE values for selection options.
+function sentenceCase (keyStr) {
+  if ((keyStr===null) || (keyStr===''))
+  return false;
+  else
+  var str = keyStr.toString();
+
+  return keyStr.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+//This is a thingamajig so I can dynamically read key values from JSON files. Will fix this to instead dynamically CREATE values for selection options.
+function addSelect (str) {
+  var strPrefix =  "selectNation";
+  var res = strPrefix.concat(str);
+  return res;
+}
+
+console.log('INIT_SELECT_NATION_OPTIONS_END');
